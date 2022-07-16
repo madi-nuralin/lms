bbaaeb5970dd2367d82d9780ba9d6b4317e8f8d4
Madi Nuralin <nmdbzk@gmail.com>
2022-07-16 22:47:10 +0600

    Update auth.php

--- resources/lang/kk/auth.php
+++ resources/lang/kk/auth.php
@@ -1,14 +1,14 @@
 <?php
 return [
-    "email" => "Эл. почта",
+    "email" => "Электрондық пошта",
     "password" => "Пароль",
-    "login" => "Войти",
-    "remember_me" => "Запомнить меня",
-    "forgot_your_password" => "Забыли пароль?",
-    "register" => "Создать аккаунт",
-    "name" => "Ваше имя",
-    "username" => "Имя пользователя",
-    "confirm_password" => "Подтвердите пароль",
+    "login" => "Кіру",
+    "remember_me" => "Мені есте сақтау",
+    "forgot_your_password" => "Құпия сөзіңізді ұмыттыңыз ба?",
+    "register" => "Аккаунты жасау",
+    "name" => "Сіздің атыңыз",
+    "username" => "Қолданушының аты",
+    "confirm_password" => "Құпия сөзді растаңыз",
     "failed" => "These credentials do not match our records.",
     "throttle" => "Too many login attempts. Please try again in :seconds seconds."
 ];
